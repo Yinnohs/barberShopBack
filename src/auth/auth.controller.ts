6da73f21176b3a6af440c0708913d3784ba4495a
josesoto <josesoto@itop.es>
2023-04-20 19:38:01 +0100

    feat(auth): created mock of service and controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,34 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
 
-@Controller('auth')
+@Controller('/api/v1/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
+  @Post('/signup')
+  async signup() {
+    return this.authService.signup();
   }
 
-  @Get()
-  findAll() {
-    return this.authService.findAll();
+  @Post('/signin')
+  async signin() {
+    return this.authService.signin();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+  @Get('/sigout')
+  async signout() {
+    return this.authService.signout();
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  async signup() {
+    return { message: 'signup good' };
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  async signin() {
+    return { message: 'signin good' };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  async signout() {
+    return { message: 'signout good' };
   }
 }
