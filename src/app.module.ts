8dda8d3f89e4162d2d1ee4e0a729b8cb3628a575
josesoto <josesoto@itop.es>
2023-04-20 19:27:27 +0100

    feat(user/auth): created users and auth modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DatabaseModule, UsersModule, AuthModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
